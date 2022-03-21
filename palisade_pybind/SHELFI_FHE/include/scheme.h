8430a2d93c9502d60b2a5a5a2260d0efce4c715a
Tanmay Ghai <tanmay.ghai@workday.com>
2022-03-21 15:15:04 -0700

    updates

--- palisade_pybind/SHELFI_FHE/include/scheme.h
+++ palisade_pybind/SHELFI_FHE/include/scheme.h
@@ -18,12 +18,12 @@ public:
         this->totalLearners = learners;
     }
 
-    virtual void loadCryptoParams();
-    virtual void genCryptoContextAndKeyGen();
-    virtual py::bytes encrypt(py::array_t<double> data_array, unsigned int iteration);
-    virtual py::bytes computeWeightedAverage(py::list learners_Data, py::list scalingFactors, int params);
-    virtual py::array_t<double> decrypt( string learner_Data, unsigned long int data_dimesions, unsigned int iteration);
-    virtual py::bytes computeWeightedAverage(py::list learners_Data, py::list scalingFactors, int params);
+    // virtual void loadCryptoParams();
+    // virtual void genCryptoContextAndKeyGen();
+    // virtual py::bytes encrypt(py::array_t<double> data_array, unsigned int iteration);
+    // virtual py::bytes computeWeightedAverage(py::list learners_Data, py::list scalingFactors, int params);
+    // virtual py::array_t<double> decrypt( string learner_Data, unsigned long int data_dimesions, unsigned int iteration);
+    // virtual py::bytes computeWeightedAverage(py::list learners_Data, py::list scalingFactors, int params);
 };
 
 PYBIND11_MODULE(SHELFI_FHE, m) {
@@ -31,12 +31,12 @@ PYBIND11_MODULE(SHELFI_FHE, m) {
 py::class_<Scheme>(m, "Scheme")
         .def(py::init<std::string &, int>(),
             py::arg("scheme") = py::str(""),
-            py::arg("learners") = 10),
-        .def("loadCryptoParams", &Scheme::loadCryptoParams)
-        .def("genCryptoContextAndKeyGen", &Scheme:genCryptoContextAndKeyGen)
-        .def("encrypt", &Scheme::encrypt)
-        .def("decrypt", &Scheme::decrypt)
-        .def("computeWeightedAverage", &Scheme::computeWeightedAverage)
+            py::arg("learners") = 10);
+        // .def("loadCryptoParams", &Scheme::loadCryptoParams)
+        // .def("genCryptoContextAndKeyGen", &Scheme:genCryptoContextAndKeyGen)
+        // .def("encrypt", &Scheme::encrypt)
+        // .def("decrypt", &Scheme::decrypt)
+        // .def("computeWeightedAverage", &Scheme::computeWeightedAverage)
 
   m.doc() = R"pbdoc(
         Pybind11 example plugin
