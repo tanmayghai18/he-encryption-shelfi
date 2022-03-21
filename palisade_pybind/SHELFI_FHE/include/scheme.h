ad4307035bbcb904aaef983fd6cfd9e72d404480
Tanmay Ghai <tanmay.ghai@workday.com>
2022-03-21 15:14:32 -0700

    updates

--- palisade_pybind/SHELFI_FHE/include/scheme.h
+++ palisade_pybind/SHELFI_FHE/include/scheme.h
@@ -1,8 +1,9 @@
-#include <pybind11/complex.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/pytypes.h>
-#include <pybind11/stl.h>
-#include <pybind11/numpy.h>
+#include <../pybind11/complex.h>
+#include <../pybind11/pybind11.h>
+#include <../pybind11/pytypes.h>
+#include <../pybind11/stl.h>
+#include <../pybind11/numpy.h>
+
 #include <omp.h>
 
 class Scheme {
