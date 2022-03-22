14b0f74de63d144c1ed28d4eebf5bb1640e1aa0f
Tanmay Ghai <tanmay.ghai@workday.com>
2022-03-21 22:27:16 -0700

    updates

--- palisade_pybind/SHELFI_FHE/include/scheme.h
+++ palisade_pybind/SHELFI_FHE/include/scheme.h
@@ -6,12 +6,6 @@
 
 #include <omp.h>
 
-#include "../src/ckks.cpp"
-
-#define STRINGIFY(x) #x
-#define MACRO_STRINGIFY(x) STRINGIFY(x)
-namespace py = pybind11;
-
 class Scheme {
 
 private:
@@ -30,42 +24,3 @@ public:
     py::bytes computeWeightedAverage(py::list learners_data, py::list scaling_factors, int params);
     py::array_t<double> decrypt( string learner_data, unsigned long int data_dimensions, unsigned int iteration);
 };
-
-PYBIND11_MODULE(SHELFI_FHE, m) {
-
-py::class_<Scheme>(m, "Scheme")
-        .def(py::init<std::string &, int>(),
-            py::arg("scheme") = py::str(""),
-            py::arg("learners") = 10)
-        .def("loadCryptoParams", &Scheme::loadCryptoParams)
-        .def("genCryptoContextAndKeyGen", &Scheme::genCryptoContextAndKeyGen)
-        .def("encrypt", &Scheme::encrypt)
-        .def("computeWeightedAverage", &Scheme::computeWeightedAverage)
-        .def("decrypt", &Scheme::decrypt);
-
-py::class_<Ckks, Scheme>(m, "Ckks")
-        .def(py::init<int, int, int, std::string &>(),
-                py::arg("batchSize") = 8192, 
-                py::arg("scaleFactorBits") = 52, 
-                py::arg("cryptodir") = py::str("../resources/cryptoparams/"))
-      .def("loadCryptoParams", &Ckks::loadCryptoParams)
-      .def("genCryptoContextAndKeyGen", &Ckks::genCryptoContextAndKeyGen);
-      // .def("encrypt", &Ckks::encrypt)
-      // .def("decrypt", &Ckks::decrypt)
-      // .def("computeWeightedAverage", &Ckks::computeWeightedAverage);
-
-  m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-        .. currentmodule:: cmake_example
-        .. autosummary::
-           :toctree: _generate
-    )pbdoc";
-
-#ifdef VERSION_INFO
-  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-  m.attr("__version__") = "dev";
-#endif
-
-}
---
+++ palisade_pybind/pythonApi/binding.cpp
@@ -0,0 +1,51 @@
+#include <pybind11/complex.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+
+#include <../include/scheme.h>
+#include <../src/ckks.cpp>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+namespace py = pybind11;
+
+PYBIND11_MODULE(SHELFI_FHE, m) {
+
+py::class_<Scheme>(m, "Scheme")
+        .def(py::init<std::string &, int>(),
+            py::arg("scheme") = py::str(""),
+            py::arg("learners") = 10)
+        .def("loadCryptoParams", &Scheme::loadCryptoParams)
+        .def("genCryptoContextAndKeyGen", &Scheme::genCryptoContextAndKeyGen)
+        .def("encrypt", &Scheme::encrypt)
+        .def("computeWeightedAverage", &Scheme::computeWeightedAverage)
+        .def("decrypt", &Scheme::decrypt);
+
+py::class_<Ckks, Scheme>(m, "Ckks")
+        .def(py::init<int, int, int, std::string &>(),
+                py::arg("batchSize") = 8192, 
+                py::arg("scaleFactorBits") = 52, 
+                py::arg("cryptodir") = py::str("../resources/cryptoparams/"))
+      .def("loadCryptoParams", &Ckks::loadCryptoParams)
+      .def("genCryptoContextAndKeyGen", &Ckks::genCryptoContextAndKeyGen);
+      // .def("encrypt", &Ckks::encrypt)
+      // .def("decrypt", &Ckks::decrypt)
+      // .def("computeWeightedAverage", &Ckks::computeWeightedAverage);
+
+  m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+        .. currentmodule:: cmake_example
+        .. autosummary::
+           :toctree: _generate
+    )pbdoc";
+
+#ifdef VERSION_INFO
+  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+  m.attr("__version__") = "dev";
+#endif
+
+}
