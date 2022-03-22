e302bce1398eaf75074b2fb8913357e5aa4c7959
Tanmay Ghai <tanmay.ghai@workday.com>
2022-03-21 22:19:23 -0700

    updates

--- palisade_pybind/SHELFI_FHE/src/ckks.cpp
+++ palisade_pybind/SHELFI_FHE/src/ckks.cpp
@@ -240,33 +240,3 @@ public:
 };
 
 
-PYBIND11_MODULE(SHELFI_FHE, m) {
-
-py::class_<Ckks, Scheme>(m, "Ckks")
-		.def(py::init<int, int, int, std::string &>(),
-                py::arg("batchSize") = 8192, 
-                py::arg("scaleFactorBits") = 52, 
-                py::arg("cryptodir") = py::str("../resources/cryptoparams/"))
-      .def("loadCryptoParams", &Ckks::loadCryptoParams)
-      .def("genCryptoContextAndKeyGen", &Ckks::genCryptoContextAndKeyGen);
-      // .def("encrypt", &Ckks::encrypt)
-      // .def("decrypt", &Ckks::decrypt)
-      // .def("computeWeightedAverage", &Ckks::computeWeightedAverage);
-
-  m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-        .. currentmodule:: cmake_example
-        .. autosummary::
-           :toctree: _generate
-    )pbdoc";
-
-#ifdef VERSION_INFO
-  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-  m.attr("__version__") = "dev";
-#endif
-
-}
-
-
