b1097e23344e93e1db8596881d674bb3c842e237
Tanmay Ghai <tanmay.ghai@workday.com>
2022-03-22 16:20:18 -0700

    refactor done

--- palisade_pybind/SHELFI_FHE/pythonApi/paillier_example.py
+++ palisade_pybind/SHELFI_FHE/pythonApi/paillier_example.py
@@ -1,3 +1,3 @@
 from SHELFI_FHE import *
 
-pailler = Pailler("pailler", 10, 2048, 17, 13, "../resources/random_params/", "../resources/cryptoparams/");
+pailler = Paillier("paillier", 10, 2048, 17, 13, "../resources/random_params/", "../resources/cryptoparams/");
