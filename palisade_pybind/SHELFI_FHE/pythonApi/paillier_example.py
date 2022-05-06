6977e15284f70717f3ac42f8fdad169140f6ee2d
Parallels <hamzasaleemzpr@gmail.com>
2022-05-05 23:49:33 -0700

    merges cpp and python libs

--- palisade_pybind/SHELFI_FHE/pythonApi/paillier_example.py
+++
@@ -1,3 +0,0 @@
-from SHELFI_FHE import *
-
-pailler = Paillier("paillier", 10, 2048, 17, 13, "../resources/random_params/", "../resources/cryptoparams/");
--- palisade_pybind/SHELFI_FHE/src/ckks.cpp
+++ palisade_pybind/SHELFI_FHE/src/ckks.cpp
@@ -1,93 +1,64 @@
-#include <pybind11/complex.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/pytypes.h>
-#include <pybind11/stl.h>
-#include <pybind11/numpy.h>
-#include <omp.h>
-
-#include "ciphertext-ser.h"
-#include "cryptocontext-ser.h"
-#include "palisade.h"
-#include "pubkeylp-ser.h"
-#include "scheme/bgvrns/bgvrns-ser.h"
-#include "scheme/ckks/ckks-ser.h"
-
-#define STRINGIFY(x) #x
-#define MACRO_STRINGIFY(x) STRINGIFY(x)
-namespace py = pybind11;
-
-using namespace std;
-using namespace lbcrypto;
-
-class Ckks : public Scheme {
-
-private:
-  string scheme;
-  int learners;
-
-	int batchSize;
-	int scaleFactorBits;
-	string cryptodir;
-
-  CryptoContext<DCRTPoly> cc;
-  LPPublicKey<DCRTPoly> pk;
-  LPPrivateKey<DCRTPoly> sk;
-
-public:
-	Ckks(string scheme, int learners, int batchSize, int scaleFactorBits, string cryptodir) : Scheme(scheme, learners) {
-    this->batchSize = batchSize;
-    this->scaleFactorBits = scaleFactorBits;
-    this->cryptodir = cryptodir;
-  }
-
-	void loadCryptoParams() override {
-		    if (!Serial::DeserializeFromFile(cryptodir + "cryptocontext.txt", cc, SerType::BINARY)) {
-        	std::cout << "Could not read serialization from " << cryptodir + "cryptocontext.txt" << std::endl;
-      	}
-
-      	if (!Serial::DeserializeFromFile(cryptodir + "key-public.txt", pk, SerType::BINARY)) {
-      		std::cout << "Could not read public key" << std::endl;
-      	}
-
-      	if (Serial::DeserializeFromFile(cryptodir + "key-private.txt", sk, SerType::BINARY) == false) {
-      		std::cerr << "Could not read secret key" << std::endl;
-      	}
-	}
-
-	int genCryptoContextAndKeyGen() override {
-        usint multDepth = 1;
-        CryptoContext<DCRTPoly> cryptoContext = CryptoContextFactory<DCRTPoly>::genCryptoContextCKKS(multDepth, scaleFactorBits, batchSize);
-        // enable features that you wish to use
-        cryptoContext->Enable(ENCRYPTION);
-        cryptoContext->Enable(SHE);
-        // cryptoContext->Enable(LEVELEDSHE);
-
-        string cryptocontext_file = "cryptocontext.txt";
-        string public_key_file = "key-public.txt";
-        string private_key_file = "key-private.txt";
+#include "ckks.h"
 
 
-        if (!Serial::SerializeToFile(cryptodir + cryptocontext_file, cryptoContext, SerType::BINARY)) {
-          std::cerr << "Error writing serialization of the crypto context to "<< cryptocontext_file << std::endl;
-          return 0;
-        }
 
-        LPKeyPair<DCRTPoly> keyPair = cryptoContext->KeyGen();
+CKKS::CKKS(string scheme, uint batchSize, uint scaleFactorBits, string cryptodir) : Scheme(scheme) {
+  this->batchSize = batchSize;
+  this->scaleFactorBits = scaleFactorBits;
+  this->cryptodir = cryptodir;
+}
 
-        if (!Serial::SerializeToFile(cryptodir + public_key_file, keyPair.publicKey, SerType::BINARY)) {
-          std::cerr << "Error writing serialization of public key to "<< public_key_file << std::endl;
-          return 0;
-        }
+void CKKS::loadCryptoParams() {
+	    if (!Serial::DeserializeFromFile(cryptodir + "cryptocontext.txt", cc, SerType::BINARY)) {
+      	std::cout << "Could not read serialization from " << cryptodir + "cryptocontext.txt" << std::endl;
+    	}
 
-        if (!Serial::SerializeToFile(cryptodir + private_key_file, keyPair.secretKey, SerType::BINARY)) {
-          std::cerr<< "Error writing serialization of private key to "<< private_key_file << std::endl;
-          return 0;
-        }
+    	if (!Serial::DeserializeFromFile(cryptodir + "key-public.txt", pk, SerType::BINARY)) {
+    		std::cout << "Could not read public key" << std::endl;
+    	}
 
-        return 1;
-	}
+    	if (Serial::DeserializeFromFile(cryptodir + "key-private.txt", sk, SerType::BINARY) == false) {
+    		std::cerr << "Could not read secret key" << std::endl;
+    	}
+}
 
-	py::bytes encrypt(py::array_t<double> data_array, unsigned int iteration) override {
+int CKKS::genCryptoContextAndKeyGen() {
+      usint multDepth = 1;
+
+      CryptoContext<DCRTPoly> cryptoContext = CryptoContextFactory<DCRTPoly>::genCryptoContextCKKS(multDepth, scaleFactorBits, batchSize);
+      
+
+      // enable features that you wish to use
+      cryptoContext->Enable(ENCRYPTION);
+      cryptoContext->Enable(SHE);
+      // cryptoContext->Enable(LEVELEDSHE);
+
+      string cryptocontext_file = "cryptocontext.txt";
+      string public_key_file = "key-public.txt";
+      string private_key_file = "key-private.txt";
+
+
+      if (!Serial::SerializeToFile(cryptodir + cryptocontext_file, cryptoContext, SerType::BINARY)) {
+        std::cerr << "Error writing serialization of the crypto context to "<< cryptocontext_file << std::endl;
+        return 0;
+      }
+
+      LPKeyPair<DCRTPoly> keyPair = cryptoContext->KeyGen();
+
+      if (!Serial::SerializeToFile(cryptodir + public_key_file, keyPair.publicKey, SerType::BINARY)) {
+        std::cerr << "Error writing serialization of public key to "<< public_key_file << std::endl;
+        return 0;
+      }
+
+      if (!Serial::SerializeToFile(cryptodir + private_key_file, keyPair.secretKey, SerType::BINARY)) {
+        std::cerr<< "Error writing serialization of private key to "<< private_key_file << std::endl;
+        return 0;
+      }
+
+      return 1;
+}
+
+	py::bytes CKKS::encrypt(py::array_t<double> data_array) {
         unsigned long int size = data_array.size();
         auto learner_Data = data_array.data();
 
@@ -132,7 +103,70 @@ public:
         return res;
 	}
 
-	py::array_t<double> decrypt(string learner_Data, unsigned long int data_dimesions, unsigned int iteration) override {
+
+  string CKKS::encrypt_cpp(vector<double> learner_Data)  {
+
+
+    unsigned long int size = learner_Data.size();
+    vector<Ciphertext<DCRTPoly>> ciphertext_data((int)((size + batchSize) / batchSize));
+
+    
+
+    if (size > (unsigned long int)batchSize) {
+
+
+        #pragma omp parallel for
+        for (unsigned long int i = 0; i < size; i += batchSize) {
+
+          unsigned long int last = std::min((long)size, (long)i + batchSize);
+
+          vector<double> batch;
+          batch.reserve(last - i + 1);
+
+          for (unsigned long int j = i; j < last; j++) {
+
+            batch.push_back(learner_Data[j]);
+          }
+
+
+          Plaintext plaintext_data = cc->MakeCKKSPackedPlaintext(batch);
+          ciphertext_data[(int)(i/batchSize)] = cc->Encrypt(pk, plaintext_data);
+
+        }
+
+      }
+
+      else {
+
+        vector<double> batch;
+        batch.reserve(size);
+
+        for (unsigned long int i = 0; i < size; i++) {
+
+          batch.push_back(learner_Data[i]);
+        }
+
+        Plaintext plaintext_data = cc->MakeCKKSPackedPlaintext(batch);
+        ciphertext_data[0] = cc->Encrypt(pk, plaintext_data);
+      }
+
+
+
+    
+
+
+    stringstream s;
+    const SerType::SERBINARY st;
+    Serial::Serialize(ciphertext_data, s, st);
+
+
+    return s.str();
+
+   
+}
+
+
+	py::array_t<double> CKKS::decrypt(string learner_Data, unsigned long int data_dimesions) {
 		    //auto start_deserialize = std::chrono::system_clock::now();
       	const SerType::SERBINARY st;
       	stringstream ss(learner_Data);
@@ -178,7 +212,55 @@ public:
     } 
 	
 
-	py::bytes computeWeightedAverage(py::list learner_data, py::list scaling_factors, int params) override {
+
+  vector<double> CKKS::decrypt_cpp(string learner_Data, unsigned long int data_dimesions) {
+
+    const SerType::SERBINARY st;
+    stringstream ss(learner_Data);
+
+    vector<Ciphertext<DCRTPoly>> learner_ciphertext;
+    Serial::Deserialize(learner_ciphertext, ss, st);
+
+    vector<double> result(data_dimesions); 
+
+
+    #pragma omp parallel for
+    for (unsigned long int i = 0; i < learner_ciphertext.size(); i++) {
+
+      Plaintext pt;
+      cc->Decrypt(sk, learner_ciphertext[i], &pt);
+      int length;
+
+      if (i == learner_ciphertext.size() - 1) {
+
+        length = data_dimesions - (i)*batchSize;
+      }
+
+      else {
+
+        length = batchSize;
+      }
+
+      pt->SetLength(length);
+      vector<double> layer_data = pt->GetRealPackedValue();
+      int m = i*batchSize;
+
+      for (unsigned long int j = 0; j < layer_data.size(); j++) {
+
+        result[m++] = layer_data[j];
+      }
+
+      
+    }
+
+
+    return result;
+
+}
+
+
+
+	py::bytes CKKS::computeWeightedAverage(py::list learner_data, py::list scaling_factors) {
         if (learner_data.size() != scaling_factors.size()) {
       		cout << "Error: learner_data and scaling_factors size mismatch" << endl;
         	return "";
@@ -235,4 +317,57 @@ public:
       	result_ciphertext.clear();
       	return res;
 	}
-};
+
+
+  string CKKS::computeWeightedAverage_cpp(vector<string> learners_Data, vector<float> scalingFactors) {
+
+  if (learners_Data.size() != scalingFactors.size()) {
+      cout << "Error: learners_Data and scalingFactors size mismatch" << endl;
+      return "";
+  }
+
+  
+
+  const SerType::SERBINARY st;
+      vector<Ciphertext<DCRTPoly>> result_ciphertext;
+
+      for (unsigned long int i = 0; i < learners_Data.size(); i++) {
+
+        stringstream ss(learners_Data[i]);
+        vector<Ciphertext<DCRTPoly>> learner_ciphertext;
+
+        Serial::Deserialize(learner_ciphertext, ss, st);
+
+        for (unsigned long int j = 0; j < learner_ciphertext.size(); j++) {
+
+          float sc = scalingFactors[i];
+          learner_ciphertext[j] = cc->EvalMult(learner_ciphertext[j], sc);
+        }
+
+        if (result_ciphertext.size() == 0) {
+
+          result_ciphertext = learner_ciphertext;
+        }
+
+        else {
+
+          for (unsigned long int j = 0; j < learner_ciphertext.size(); j++) {
+
+            result_ciphertext[j] = cc->EvalAdd(result_ciphertext[j], learner_ciphertext[j]);
+          }
+        }
+
+
+      }
+
+
+      stringstream ss;
+      Serial::Serialize(result_ciphertext, ss, st);
+      return ss.str();
+
+
+    
+}
+
+
+
